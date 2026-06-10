c770964b492004c500339c6dee9b01e978f377db
boye1026 <490159362@qq.com>
2026-06-10 18:01:38 +0800

    Update child_info_service.dart

--- mobile/lib/services/child_info_service.dart
+++ mobile/lib/services/child_info_service.dart
@@ -5,14 +5,12 @@ import '../models/child_info.dart';
 class ChildInfoService {
   static const String _storageKey = 'child_info';
 
-  // 保存孩子信息
   Future<void> saveChildInfo(ChildInfo childInfo) async {
     final prefs = await SharedPreferences.getInstance();
     final jsonString = jsonEncode(childInfo.toJson());
     await prefs.setString(_storageKey, jsonString);
   }
 
-  // 获取孩子信息
   Future<ChildInfo?> getChildInfo() async {
     final prefs = await SharedPreferences.getInstance();
     final jsonString = prefs.getString(_storageKey);
@@ -29,13 +27,11 @@ class ChildInfoService {
     }
   }
 
-  // 删除孩子信息
   Future<void> deleteChildInfo() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_storageKey);
   }
 
-  // 检查是否已设置信息
   Future<bool> hasChildInfo() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.containsKey(_storageKey);
